84626ff4ddc9f05c30983bf6fd6afbfa988a3497
Wonhee Han <hanwh@hunature.net>
2020-10-04 00:49:07 +0900

    list sample

--- sample/list.py
+++ sample/list.py
@@ -11,3 +11,23 @@ len_list_if_filtering = [len(word) for word in words if len(word) > 3]
 print(len_list_if_filtering)
 
 print(len(len_list_if_filtering))
+
+my_list = [1, '2', 3, '4', 5, [6, 7], 8, 9]
+
+my_list.append('10')
+print(my_list)
+print(len(my_list))
+
+my_list.append(1)
+print(my_list)
+print(len(my_list))
+
+my_list.remove(1)
+my_list.remove(1)
+
+print(my_list)
+print(len(my_list))
+print(my_list.index(8))
+
+my_list.reverse() #my_list.sort()
+print(my_list)
