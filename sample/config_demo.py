3452d286d96e11eff9b91ad7a98f7a2e4b8cac12
Wonhee Han <hanwhhanwh@gmail.com>
2020-10-06 14:31:56 +0900

    avoid SectionError, KeyError

--- sample/config_demo.py
+++ sample/config_demo.py
@@ -11,4 +11,32 @@ config = configparser.ConfigParser()
 
 config.read('./config/db.ini')
 sec = config.sections()
-print(sec)
+print('sections() = ', sec)
+
+if ("DB" in config):
+	print("\n'DB' in config")
+	db = config["DB"]
+	print(db)
+
+if (config.has_section("DB")):
+	print("\nconfig.has_section('DB')")
+	items = config.items("DB")
+	print(items)
+
+	options = config.options("DB")
+	print(options)
+
+if (config.has_option("DB", "host")):
+	print("\nconfig.has_option('DB', 'host')")
+	host = config.get("DB", "host")
+	print(host)
+
+if ("db_name" in db):
+	print("\n'db_name' in db")
+	print(db["db_name"])
+
+if ("option_nothing" in db):
+	print(db["option_nothing"])
+
+# occur >> KeyError: 'option_nothing'
+print(db["option_nothing"])
