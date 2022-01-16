1182662315ccf80a217b5520e4537595d62b19b8
Wonhee Han <hanwhhanwh@gmail.com>
2022-01-16 17:50:14 +0900

    add thumb image url

--- SQL/AVDB/FILM_STILL_IMAGES.sql
+++ SQL/AVDB/FILM_STILL_IMAGES.sql
@@ -4,6 +4,7 @@ CREATE TABLE `FILM_STILL_IMAGES`
 	`film_still_image_no`		INT NOT NULL AUTO_INCREMENT COMMENT '작품 스틸 이미지 고유번호'
 	, `film_no`					INT NOT NULL COMMENT '작품 고유번호 (FK: FILM.film_no)'
 	, `still_image_url`			VARCHAR(255) NOT NULL COMMENT '작품 스틸 이미지 주소 정보' COLLATE 'utf8_general_ci'
+	, `still_image_thumb_url`	VARCHAR(255) NOT NULL COMMENT '썸브용 작품 스틸 이미지 주소 정보' COLLATE 'utf8_general_ci'
 	, `width`					SMALLINT NULL COMMENT '이미지 폭'
 	, `height`					SMALLINT NULL COMMENT '이미지 높이'
 
