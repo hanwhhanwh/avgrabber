b20ab7297f76b7d3496dc0daa706327aa82cc235
Wonhee Han <hanwhhanwh@gmail.com>
2024-01-07 01:32:45 +0900

    MOD: 컬럼 데이터 변환 처리

--- SQL/AVDB/AVGOSU.sql
+++ SQL/AVDB/AVGOSU.sql
@@ -2,14 +2,14 @@
 CREATE TABLE `AVGOSU`
 (
 	`avgosu_no`				INT NOT NULL AUTO_INCREMENT COMMENT 'AVGOSU에서 수집한 정보의 고유번호'
-	, `detail_url`			VARCHAR(255) NOT NULL COMMENT 'AVGOSU 상세정보 주소' COLLATE 'utf8mb4_unicode_ci'
+	, `avgosu_board_no`		INT NOT NULL COMMENT 'AVGOSU etc 에서의 고유 번호'
 	, `film_id`				VARCHAR(50) NOT NULL COMMENT '품번' COLLATE 'utf8mb4_unicode_ci'
 	, `title`				VARCHAR(500) NOT NULL COMMENT '제목' COLLATE 'utf8mb4_unicode_ci'
 	, `avgosu_date`			DATETIME NOT NULL COMMENT 'AVGOSU 기준 등록일'
 	, `file_size`			VARCHAR(50) NOT NULL COMMENT '파일 용량'
-	, `cover_image_url`		VARCHAR(255) NULL COMMENT '표지 이미지 주소 정보' COLLATE 'utf8mb4_unicode_ci'
-	, `thumbnail_url`		VARCHAR(255) NULL COMMENT '썸네일 이미지 주소 정보' COLLATE 'utf8mb4_unicode_ci'
-	, `magnet_addr`			VARCHAR(200) NULL COMMENT '마그넷 주소' COLLATE 'utf8mb4_unicode_ci'
+	, `cover_image_url`		VARCHAR(255) NULL COMMENT '표지 이미지 주소 정보 (\'/uploads/images\' 를 앞에 붙여야 함)' COLLATE 'utf8mb4_unicode_ci'
+	, `thumbnail_url`		VARCHAR(255) NULL COMMENT '썸네일 이미지 주소 정보 (\'/uploads/images\' 를 앞에 붙여야 함)' COLLATE 'utf8mb4_unicode_ci'
+	, `magnet_info`			VARBINARY(50) NULL DEFAULT NULL COMMENT '마그넷 주소 정보'
 	, `resolution`			TINYINT NOT NULL DEFAULT 0 COMMENT '화면 해상도: "F"=FHD, "H"=HD, "S"=SD, "4"=4K'
 	, `reg_date`			DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP() COMMENT '정보 수집시각'
 
